a291fb3b8b6c00f5329800a8b8bedf93be065826
Henry Arcila <helpu1409@gmail.com>
2024-07-22 20:19:53 -0500

    Actualizando repo

--- content/local/code/colas/cola_array.c
+++ content/local/code/colas/cola_array.c
@@ -7,8 +7,8 @@
 
 typedef struct _queue {
     int count;
-    int front;
-    int rear;
+    int head;
+    int tail;
     int list[CAPACITY];
 } queue;
 
@@ -16,80 +16,62 @@ void Queue_init(queue *q);
 int Queue_isEmpty(queue *q); 
 int Queue_isFull(queue *q);
 int Queue_size(queue *q);
-int Queue_front(queue *q);
+int Queue_head(queue *q);
 int Queue_back(queue *q);
 void Queue_enqueue(queue *q,  int data);
 int Queue_dequeue(queue *q, int *data);
 void Queue_print(queue *q);
+void Queue_clear(queue *q);
+
 
 int main(int argc, char *argv[]) {
     queue Q;
-    Queue_init(&Q);
+    Queue_init(&Q);                      // La cola creada tiene 4 elementos --> [] -->
     if(Queue_isEmpty(&Q)) {
         printf("La cola esta vacia\n");
     }
-    else {
-        printf("La cola tiene elementos\n");
+    else if(Queue_isFull(&Q)) {
+        printf("La cola esta llena\n");
     }
-    printf("Tamaño Q: %d\n",Queue_size(&Q));
+    printf("Tamaño de la cola Q: %d\n",Queue_size(&Q));
     // Agregando elementos a la cola
-    
-    Queue_enqueue(&Q, 19);
-    Queue_enqueue(&Q, 45);
-    Queue_enqueue(&Q, 13);
-    Queue_enqueue(&Q, 7);
+    Queue_enqueue(&Q, 19);               // --> [19] -->
+    Queue_enqueue(&Q, 45);               // --> [45, 19] -->
+    Queue_enqueue(&Q, 13);               // --> [13, 45, 19] -->
+    Queue_enqueue(&Q, 7);                // --> [7, 13, 45, 19] -->
+    // Despliegue de la cola
     Queue_print(&Q);
-    printf("Primero: %d\n",Queue_front(&Q));
-    printf("Ultimo: %d\n",Queue_back(&Q));
+    // Mostrando el primer y ultimo elemento de la cola
+    printf("Primer elemento de la cola: %d\n",Queue_head(&Q));
+    printf("Ultimo elemento de la cola: %d\n",Queue_back(&Q));
+    // Verificando si la cola esta llena
+    if(Queue_isFull(&Q)) {
+        printf("La cola esta llena\n");
+    }
+    // Encolando un dato cuando la cola esta llena
     Queue_enqueue(&Q, 21);
-    int data;
-    Queue_dequeue(&Q, &data);
-    printf("Dato sacado: %d\n",data);
+    // Sacando un dato de la cola (desencolar)
+    int data;  // Valor en el que se llevara el dato sacado.
+    Queue_dequeue(&Q, &data);            // --> [7, 13, 45] -->
+    printf("Dato sacado de la cola: %d\n",data);
+    // Despliegue de la cola
     Queue_print(&Q);
-    Queue_enqueue(&Q, 21);
+    // Encolando un nuevo dato
+    data = 21;
+    printf("Metiendo el %d a la cola \n", data);
+    Queue_enqueue(&Q, data);              // --> [21, 7, 13, 45] -->
     Queue_print(&Q);
-
-    /*
-    printf("Tamaño Q: %d\n",Queue_size(Q));
-    // Agregando elementos a la cola
-    Queue_enqueue(Q, 19);
-    Queue_enqueue(Q, 45);
-    Queue_enqueue(Q, 13);
-    Queue_enqueue(Q, 7);
-    printf("Tamaño Q: %d\n",Queue_size(Q));
-    // Queue_print(Q);
-    int data;
-    Queue_dequeue(Q, &data);
-    printf("Dato sacado: %d\n",data);
-    //Queue_print(Q);    
-    Queue_dequeue(Q, &data);
-    printf("Dato sacado: %d\n",data);
-    //Queue_print(Q);    
-    Queue_dequeue(Q, &data);
-    printf("Dato sacado: %d\n",data);
-    //Queue_print(Q);    
-    Queue_dequeue(Q, &data);
-    printf("Dato sacado: %d\n",data);
-    //Queue_print(Q);    
-    Queue_dequeue(Q, &data);
-    printf("Dato sacado: %d\n",data);
-    //Queue_print(Q);
-    */
-
-    /*
-    Queue_init(&Q);
-    Queue_print(&Q);
-    Queue_enqueue(&Q, 1);
-    Queue_enqueue(&Q, 2);
-    Queue_print(&Q);
-    */
+    // Vaciando la cola
+    Queue_clear(&Q);
+    if(Queue_isEmpty(&Q)) {
+        printf("La cola esta vacia\n");
+    }
     return 0;
 }
 
-
 void Queue_init(queue *q) {
-    q->front = 0;
-    q->rear = CAPACITY - 1;
+    q->head = 0;
+    q->tail = CAPACITY - 1;
     q->count = 0;
 }
 
@@ -105,21 +87,21 @@ int Queue_size(queue *q) {
     return CAPACITY;
 }
 
-int Queue_front(queue *q) {
+int Queue_head(queue *q) {
     assert(!Queue_isEmpty(q));
-    return q->list[q->front];
+    return q->list[q->head];
 }
 
 int Queue_back(queue *q) {
     assert(!Queue_isEmpty(q));
-    return q->list[q->rear];
+    return q->list[q->tail];
 }
 
 void Queue_enqueue(queue *q,  int data) {  
   if(!Queue_isFull(q)) {
-    q->rear = (q->rear + 1) % CAPACITY; 
+    q->tail = (q->tail + 1) % CAPACITY; 
     q->count++;
-    q->list[q->rear] = data;   
+    q->list[q->tail] = data;   
   }
   else {
     printf("ERROR: Full queue\n");
@@ -130,8 +112,8 @@ void Queue_enqueue(queue *q,  int data) {
 int Queue_dequeue(queue *q, int *data) {
     if (!Queue_isEmpty(q)) {
         q->count--;
-        *data = q->list[q->front];
-        q->front = (q->front + 1) % CAPACITY;
+        *data = q->list[q->head];
+        q->head = (q->head + 1) % CAPACITY;
         return 0;
     }
     else {
@@ -146,10 +128,10 @@ void Queue_print(queue *q) {
         printf("Empty queue.\n");
     }
     else {
-        // front - rear
-        if(q->front < q->rear) {
+        // head - tail
+        if(q->head < q->tail) {
             printf("<-- ");
-            for(int i = q->front; i <= q->rear; i++){
+            for(int i = q->head; i <= q->tail; i++){
                 printf("%4d    ", q->list[i]);
             }
             printf("<-- \n");            
@@ -157,30 +139,19 @@ void Queue_print(queue *q) {
         else {
             int i;
             printf("<-- ");
-            for(i = q->front; i < CAPACITY; i++){
+            for(i = q->head; i < CAPACITY; i++){
                 printf("%4d    ", q->list[i]);
             }
-            for(i = 0; i <= q->rear; i++) {
+            for(i = 0; i <= q->tail; i++) {
                 printf("%4d    ", q->list[i]);
             }
             printf("<-- \n");    
         }
     }
-    /*
-    node *current = q->head;
-    if (current == NULL) {
-        printf("Empty queue.\n");
-        return;
-    } 
-    else { 
-        printf("<-- ");
-        while (current) {
-            if(current) { 
-                printf("%4d    ", current->data);
-            }
-            current = current->next;
-        } 
-        printf("<-- \n");
-    } 
-    */
 }
+
+void Queue_clear(queue *q) {
+  q->head = 0;
+  q->tail = CAPACITY - 1;
+  q->count = 0;
+} 
